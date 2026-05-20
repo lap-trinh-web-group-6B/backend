b2f2c87bbdb56a5acf446592a04f768655b424a3
Uyen <ngocuyenle15@gmail.com>
2026-05-20 09:39:38 +0700

    chore: init code base

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+
+dotenv.config();
+const app = express();
+
+
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`[Server] Web App chạy ở API: http://localhost:${PORT}`);
+});
+
