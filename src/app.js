edb79a3d352447f1df8fdb9064f1a442ecfffa32
Lê Ngọc Uyển <ngocuyenle15@gmail.com>
2026-05-20 10:48:16 +0700

    fix(database): fix connection database

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import {prisma} from './config/database.js'
+
+
 
 
 
 dotenv.config();
 const app = express();
-
-
-
 
 const PORT = process.env.PORT || 3000;
 
