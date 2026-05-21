e7e3d9489801c7bce1a7bfc2734b0324a187c917
Lê Ngọc Uyển <ngocuyenle15@gmail.com>
2026-05-21 13:57:43 +0700

    feat: implement update api for user (name, status, change-password)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import {jsonResponse} from '../utils/responseHelper.js';
-import {prisma} from "../config/database.js";
+import {prisma} from '../config/database.js';
+import * as argon2 from 'argon2';
 
 export const userController= {
     getProfile: async (req, res) => {
@@ -15,6 +16,96 @@ export const userController= {
             }
             const {password, syncId, createdAt, ...userData} = user;
             return jsonResponse(res, 200, 'Thành công', userData);
+        } catch (error) {
+            return jsonResponse(res, 500, 'Lỗi hệ thống', null);
+        }
+    },
+    updateName: async (req, res) => {
+        try {
+            const {fullName} = req.body;
+            if (!fullName || typeof fullName !== 'string' || fullName.trim() === '') {
+                return jsonResponse(res, 400, 'Lỗi', {fullName: 'Tên không hợp lệ'});
+            }
+            const user = await prisma.users.findUnique({
+                where: {
+                    id: req.user.id
+                }
+            });
+            if (!user) {
+                return jsonResponse(res, 404, 'Không tìm thấy người dùng', null);
+            }
+            user.fullName = fullName.trim();
+            await prisma.users.update({
+                where: {
+                    id: user.id
+                },
+                data: user
+            });
+            const {password, syncId, createdAt, ...updatedUser} = user;
+            return jsonResponse(res, 200, 'Thành công', updatedUser);
+        } catch (error) {
+            return jsonResponse(res, 500, 'Lỗi hệ thống', null);
+        }
+
+    },
+    updateStatus: async (req, res) => {
+        try {
+            const {status} = req.body;
+            if (!['ACTIVATE', 'DISABLE', 'CANCEL'].includes(status)) {
+                return jsonResponse(res, 400, 'Lỗi', {status: 'Trạng thái không hợp lệ'});
+            }
+            const user = await prisma.users.findUnique({
+                where: {
+                    id: req.user.id
+                }
+            });
+            if (!user) {
+                return jsonResponse(res, 404, 'Không tìm thấy người dùng', null);
+            }
+            user.status = status;
+            await prisma.users.update({
+                where: {
+                    id: user.id
+                },
+                data: user
+            });
+            return jsonResponse(res, 200, 'Thành công');
+        } catch (error) {
+            return jsonResponse(res, 500, 'Lỗi hệ thống', null);
+        }
+
+    },
+    changePassword: async (req, res) => {
+        try {
+            const {currentPassword, newPassword, confirmPassword} = req.body;
+            if (!currentPassword || !newPassword || !confirmPassword) {
+                return jsonResponse(res, 400, 'Vui lòng nhập đầy đủ thông tin', null);
+            }
+
+            const user = await prisma.users.findUnique({
+                where: {
+                    id: req.user.id
+                }
+            });
+            if (!user) {
+                return jsonResponse(res, 404, 'Không tìm thấy người dùng', null);
+            }
+
+            const isPasswordValid = await argon2.verify(user.password, currentPassword);
+            if (!isPasswordValid) {
+                return jsonResponse(res, 400, 'Lỗi', {currentPassword: 'Mật khẩu hiện tại không đúng'});
+            }
+            if (newPassword !== confirmPassword) {
+                return jsonResponse(res, 400, 'Lỗi', {confirmPassword: 'Mật khẩu xác nhận không khớp'});
+            }
+            user.password = await argon2.hash(newPassword);
+            await prisma.users.update({
+                where: {
+                    id: user.id
+                },
+                data: user
+            });
+            return jsonResponse(res, 200, 'Thành công');
         } catch (error) {
             return jsonResponse(res, 500, 'Lỗi hệ thống', null);
         }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,4 +6,10 @@ const router = express.Router();
 
 router.get('/profile', requireAuth, userController.getProfile);
 
+router.patch('/update-name', requireAuth, userController.updateName);
+
+router.patch('/update-status', requireAuth, userController.updateStatus);
+
+router.patch('/change-password', requireAuth, userController.changePassword);
+
 export default router;
