2103f2d893eb277269e99f6699b750f5c4233ae1
Lê Ngọc Uyển <ngocuyenle15@gmail.com>
2026-06-14 10:50:04 +0700

    refactor

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/uploads', express.static('uploads'));
 app.use('/api/v1', apiRoutes);
 
 app.use('/api/payment', paymentRoutes);
